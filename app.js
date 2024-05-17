9e69555abef1e33a9bb415d55f48bfda7952578c
elic4vet <86532060+elic4vet@users.noreply.github.com>
2024-05-17 22:59:07 +0200

    Update app.js
    fixed the date and the location

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const name = document.getElementById('name');
+const profileName = document.getElementById('name');
 const username = document.getElementById('username');
 const bio = document.getElementById('bio');
 const btn = document.getElementById('btn');
@@ -7,10 +7,12 @@ const image = document.getElementById('image');
 const followers = document.getElementById('followers');
 const following = document.getElementById('following');
 const repos = document.getElementById('repos');
+const registration = document.getElementById('registration');
+
 const website = document.getElementById('website');
 const twitter = document.getElementById('twitter');
-const company = document.getElementById('company');
-const registration = document.getElementById('registration');
+const workplace = document.getElementById('workplace');
+const userLocation = document.getElementById('location');
 
 input.addEventListener('input', (e) => {
     console.log(e.target.value);
@@ -20,15 +22,19 @@ btn.addEventListener('click', () => {
     fetch(`https://api.github.com/users/${input.value}`)
         .then(response => response.json())
         .then(data => {
-            name.innerHTML = data.name;
-            username.innerHTML = data.login;
-            bio.innerHTML = data.bio;
+            console.log(data);
+            profileName.textContent = data.name;
+            username.textContent = data.login;
+            bio.textContent = data.bio;
             image.src = data.avatar_url;
-            followers.innerHTML = data.followers;
-            following.innerHTML = data.following;
-            repos.innerHTML = data.public_repos;
-            location.innerHTML = data.location;
-        }
-        );
-}
-);
+            followers.textContent = data.followers;
+            following.textContent = data.following;
+            repos.textContent = data.public_repos;
+            const createdAt = new Date(data.created_at);
+            registration.textContent = data.created_at ? createdAt.toLocaleDateString() : 'N/A';
+            website.textContent = data.blog;
+            twitter.textContent = data.twitter_username;
+            workplace.textContent = data.company;
+            userLocation.textContent = data.location;
+        });
+}); 
